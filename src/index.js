deff77c4268f331fa549caa948a65cbab528a052
Mdakhib <mohamedakhib@gmail.com>
2021-06-20 12:51:31 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
+//a4ebaac74ae54227b0f45e8e4a74aebe
