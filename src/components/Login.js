d433d72a900e9be8fd5babd5bfcd1f39597379fc
Mdakhib <mohamedakhib@gmail.com>
2021-06-20 13:17:29 +0530

    login file created

---
+++ src/components/Login.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../login.css'
+
+function Login() {
+    return (
+        <div>
+            {/* spotify logo */}
+            
+            {/* Login  with spotify btn */}
+        </div>
+    )
+}
+
+export default Login
