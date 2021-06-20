4d1d2be9be90900f7e925be9685d215b4777ef11
Mdakhib <mohamedakhib@gmail.com>
2021-06-20 16:34:13 +0530

    we are taking token in to page.if token is there loggin or show signin page by using ternary opt

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import React, {useEffect} from 'react';
+import React, {useEffect,useState} from 'react';
 import './App.css';
 import { Login } from "./components";
 import { getTokenFromUrl } from './spotify';
 
 
 function App() {
-  return (
-    <div>
-      <Login />
-    </div>
-  );
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token);
+    }
+
+    console.log(`I Have a token`, token);
+  }, []);
+
+  return <div>{token ? <h1>I'm logged in</h1> : <Login />}</div>;
 }
 
 export default App;
