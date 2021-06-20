886892274548677ae22a962d04335a6ee7fb7883
Mdakhib <mohamedakhib@gmail.com>
2021-06-20 16:21:07 +0530

    when login button tapped it redirects to the spotify login page. when it authorised user then it redirects to the home page and return the token in url

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import React, {useEffect} from 'react';
 import './App.css';
 import { Login } from "./components";
+import { getTokenFromUrl } from './spotify';
+
 
 function App() {
+
+  const [token,setToken]
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = '';
+    const token = hash.access_token;
+    console.log(`I Have a token` ,token);
+  },[])
+
+
   return (
     <div>
       <Login />
