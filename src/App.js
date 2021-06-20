1332cd1c829251da576ada29eca84e428281ab9d
Mdakhib <mohamedakhib@gmail.com>
2021-06-20 16:57:21 +0530

    spotify-web-api-js pacakge install to support sopotify by accessing token in which it gives the deatil of user

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import React, {useEffect,useState} from 'react';
-import './App.css';
-import { Login } from "./components";
-import { getTokenFromUrl } from './spotify';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { Login, Player } from "./components";
+import { getTokenFromUrl } from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js";
 
+const spotify = new SpotifyWebApi();
 
 function App() {
   const [token, setToken] = useState(null);
@@ -11,15 +14,19 @@ function App() {
     const hash = getTokenFromUrl();
     window.location.hash = "";
     const _token = hash.access_token;
-
+    
     if (_token) {
-      setToken(_token);
+    setToken(_token);
+      spotify.setAccessToken(_token);
+      spotify.getMe().then((user) => {
+        console.log("MD", user);
+      });
     }
 
     console.log(`I Have a token`, token);
   }, []);
 
-  return <div>{token ? <h1>I'm logged in</h1> : <Login />}</div>;
+  return <div>{token ? <Player /> : <Login />}</div>;
 }
 
 export default App;
---
+++ src/components/Player.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/player.css'
+
+function Player() {
+    return (
+        <div>
+            Hai this is Player
+        </div>
+    )
+}
+
+export default Player
