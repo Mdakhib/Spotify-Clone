7dddb6f1c9716bf1e810df62eec210e761990680
Mdakhib <mohamedakhib@gmail.com>
2021-06-20 13:31:29 +0530

    login page styled

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,11 @@
 import React from 'react'
-import '../login.css'
+import '../styles/login.css'
 
 function Login() {
     return (
-        <div>
-            {/* spotify logo */}
-            
-            {/* Login  with spotify btn */}
+        <div className='login'>
+            <img src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt="" />
+            <a>LOGIN WITH SPOTIFY</a>
         </div>
     )
 }
