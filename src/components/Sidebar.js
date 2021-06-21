f2dd2712ef428aa5e5be25466f9e26e9fa8aedf0
Mdakhib <mohamedakhib@gmail.com>
2021-06-21 17:14:17 +0530

    side bar titles and icons added

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,13 +1,26 @@
+import { Home, LibraryMusic, Search } from '@material-ui/icons';
 import React from 'react'
 import '../styles/sidebar.css'
+import SidebarOption from './SidebarOption';
 
 function Sidebar() {
     return (
       <div className="sidebar">
-        <img className='sidebar__logo'
+        <img
+          className="sidebar__logo"
           src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
           alt=""
         />
+        <SidebarOption Icon={Home} title="Home" />
+        <SidebarOption Icon={Search} title="Search" />
+        <SidebarOption Icon={LibraryMusic} title="Your Library" />
+        <br />
+        <strong className="sidebar__title">PLAYLISTS</strong>
+        <hr />
+
+        <SidebarOption title="Hip Hop" />
+        <SidebarOption title="Rock" />
+        <SidebarOption title="RnB" />
       </div>
     );
 }
