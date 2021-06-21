1116f5fe9c40e097622ea2f9ef7a7989c4f7174a
Mdakhib <mohamedakhib@gmail.com>
2021-06-21 14:35:18 +0530

    contextApi nd useReducer is used to pass the data from DataLayer to index.js useing props

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { DataLayer } from "./DataLayer";
+import reducer, { initialState } from "./reducer";
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <DataLayer initialState={initialState} reducer={reducer} >
+      <App />
+    </DataLayer>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 
