2c528c4c05f7bb2d61b8eaf319db2700cdb5c6f7
Mdakhib <mohamedakhib@gmail.com>
2021-06-21 19:08:52 +0530

    footer created and styled

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,48 @@
-import React from 'react'
-import '../styles/footer.css'
+import { Grid, Slider } from "@material-ui/core";
+import {
+  PlayCircleOutline,
+  PlaylistPlay,
+  Repeat,
+  Shuffle,
+  SkipNext,
+  SkipPrevious,
+  VolumeDown,
+} from "@material-ui/icons";
+import React from "react";
+import "../styles/footer.css";
 
 function Footer() {
-    return (
-        <div className='footer'>
-            <h1>I'm Footer </h1>
+  return (
+    <div className="footer">
+      <div className="footer__left">
+        <img className="footer__albumLogo" src="/images/download.jfif" alt="" />
+        <div className="footer__songInfo">
+          <h4>Taaron ke shehar</h4>
+          <p>Neha</p>
         </div>
-    )
+      </div>
+      <div className="footer__center">
+        <Shuffle className="footer__green" />
+        <SkipPrevious className="footer__icon" />
+        <PlayCircleOutline className="footer__icon" fontSize="large" />
+        <SkipNext className="footer__icon" />
+        <Repeat className="footer__green" />
+      </div>
+      <div className="footer__right">
+        <Grid container spacing={2}>
+          <Grid item>
+            <PlaylistPlay />
+          </Grid>
+          <Grid item>
+            <VolumeDown />
+          </Grid>
+          <Grid item xs>
+            <Slider aria-labelledby="continuous-slider" />
+          </Grid>
+        </Grid>
+      </div>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
