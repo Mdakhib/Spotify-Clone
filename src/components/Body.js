3897c4c0ba57ff88fdcf0abeeb6c2cd066a67624
Mdakhib <mohamedakhib@gmail.com>
2021-06-21 21:02:18 +0530

    songRow section added and styled and data fetched using context and useReducer

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,36 +3,38 @@ import React from 'react'
 import { useDataLayerValue } from '../DataLayer'
 import '../styles/body.css'
 import Header from './Header'
+import SongRow from './SongRow';
 
 function Body({ spotify }) {
     
     const [{ discover_weekly }, dispatch] = useDataLayerValue();
 
     return (
-        <div className='body'>
-            <Header spotify={spotify} />
+      <div className="body">
+        <Header spotify={spotify} />
 
-            <div className="body__info">
-                <img  src={discover_weekly?.images[0].url} alt="" />
-                <div className="body__infoText">
-                    <strong>PLAYLIST</strong>
-                    <h2>Discover Weekly</h2>
-                    <p>{discover_weekly?.description} </p>
-                </div>
-            </div>
-
-
-            <div className="body_songs">
-                <div className="body__icons">
-                    <PlayCircleFilled className='body__shuffle'/>
-                    <Favorite fontSize='large' />
-                    <MoreHoriz />
-                </div>
-            </div>
-
-
+        <div className="body__info">
+          <img src={discover_weekly?.images[0].url} alt="" />
+          <div className="body__infoText">
+            <strong>PLAYLIST</strong>
+            <h2>Discover Weekly</h2>
+            <p>{discover_weekly?.description} </p>
+          </div>
         </div>
-    )
+
+        <div className="body_songs">
+          <div className="body__icons">
+            <PlayCircleFilled className="body__shuffle" />
+            <Favorite fontSize="large" />
+            <MoreHoriz />
+          </div>
+
+          {discover_weekly?.tracks.items.map((item) => (
+            <SongRow track={item.track} />
+          ))}
+        </div>
+      </div>
+    );
 }
 
 export default Body;
